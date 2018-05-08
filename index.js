60d794077a4420f878f6d1696e101d98755cf604
Grey Fey <goblinlord@greyfey.com>
2018-05-08 14:54:48 -0700

    Basic streem

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Still haven't started yet. Just setting up my environment.
+const Steem = require('steem');
+Steem.api.streamBlock(function (err, block) {
+    block.transactions.forEach(transaction => {
+      if(transaction.operations[0][0]==='transfer'
+  ) {
+        const {timestamp, block_id} = block;
+        const value = {
+          trx_id: transaction.transaction_id,
+          block: block_id,
+          timestamp: timestamp,
+          op: transaction.operations[0],
+        }
+        console.log(value);
+      }
+    });
+  });
